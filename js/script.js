a996fd6286875f7461124f0cc2c5acb5b66a4bf5
ediesalasmiller <98842098+ediesalasmiller@users.noreply.github.com>
2022-02-11 11:08:05 -0800

    Pokemon List

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
-alert('Hello world');
-let favoriteFood = "Strawberries";
-document.write(favoriteFood);
+let pokemonList = [
+    {name: "Squirtle", height: 0.5, type: ['grass', 'electric']},
+    {name: "Jigglypuff", height: 0.5, type: ['poison', 'steel']},
+    {name: "Charmander", height: 0.6, type: 'ground'}
+]
