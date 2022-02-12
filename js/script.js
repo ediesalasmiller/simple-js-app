5725da521aab5a46595662b52fe72e94b51644e0
ediesalasmiller <98842098+ediesalasmiller@users.noreply.github.com>
2022-02-12 14:35:59 -0800

    Added Conditionals to pokemon

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
 let pokemonList = [
-    {name: "Squirtle", height: 0.5, type: ['grass', 'electric']},
-    {name: "Jigglypuff", height: 0.5, type: ['poison', 'steel']},
-    {name: "Charmander", height: 0.6, type: 'ground'}
-]
+    { 
+        name: "Squirtle", 
+        height: 0.5, 
+        type: ['grass', 'electric']
+    },
+    { 
+        name: "Jigglypuff", 
+        height: 0.5, 
+        type: ['poison', 'steel']
+    },
+    { 
+        name: "Charmander", 
+        height: 0.6, 
+        type: ['ground']
+    }
+];
+
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height >0.5){
+        document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ' + 'Not a tiny pokemon!');
+}else {
+    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ');
+}
+}
