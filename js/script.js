508874c79ee52b3f474bd371b3224aa2bca9bb07
ediesalasmiller <98842098+ediesalasmiller@users.noreply.github.com>
2022-02-16 13:34:26 -0800

    IIFE reformatting

--- js/script.js
+++ js/script.js
@@ -1,20 +1,35 @@
-let pokemonList = [
-    { 
-        name: "Squirtle", 
-        height: 0.5, 
-        type: ['grass', 'electric']
-    },
-    { 
-        name: "Jigglypuff", 
-        height: 0.5, 
-        type: ['poison', 'steel']
-    },
-    { 
-        name: "Charmander", 
-        height: 0.6, 
-        type: ['ground']
-    }
-];
+let pokemonRepository = (function () {
+    pokemonList = [
+        { 
+            name: "Squirtle", 
+            height: 0.5, 
+            type: ['grass', 'electric']
+        },
+        { 
+            name: "Jigglypuff", 
+            height: 0.5, 
+            type: ['poison', 'steel']
+        },
+        { 
+            name: "Charmander", 
+            height: 0.6, 
+            type: ['ground']
+        }
+    ];
+
+        return  {
+            add: function(pokemon) {
+                pokemonList.push(pokemon)
+            },
+            getAll: function() {
+                return pokemonList
+            }
+        };
+    })();
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({ name: 'Bulbasaur', height: 2.4, type: ['grass', 'posion']  });
+console.log(pokemonRepository.getAll());
+
 
 function myLoopFunction(pokemon) {
     console.log('Name: ' + pokemon.name + ' Height: ' + pokemon.height + ' Type: ' + pokemon.type);
